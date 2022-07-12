71fbb61c7fe7c06ec13bc923e9a8e5c53efd928f
Mohamed Said <47281180+AlphaArslan@users.noreply.github.com>
2022-07-12 20:24:29 +0200

    Time: 4 ms (45.51%), Space: 6.3 MB (6.49%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.cpp
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     int reverse(int x) {
-        // handle the special case of zero
-        if( x == 0)
+        // handle the special case of zero and -2147483648
+        if( x == 0 || x == -2147483648 )
         {
             return 0;
         }
         
         // get the absolute value of x
-        long int abs_x = abs(x);
+        int abs_x = abs(x);
+        short sign = ( x >= 0 )? 1 : -1;
         
         // how many digits in abs_x
         int digits = log10(abs_x) + 1;
 
         // decompose the number into a vector of its digits in reverse
-        vector<short> digits_vec(digits, 0);
-        for(int i = 0; i < digits && abs_x != 0; i++)
+        vector<short> digits_vec(digits);
+        for(int i = 0; i < digits; i++)
         {
             digits_vec[i] = abs_x % 10;
             abs_x /= 10;
@@ -24,15 +25,7 @@ public:
         // check if the reversed number is out of range
         if(digits == 10)
         {
-            vector<short> max(10);
-            if( x >= 0 )
-            {
-                max = {2, 1, 4, 7, 4, 8, 3, 6, 4, 7};
-            }
-            else
-            {
-                max = {2, 1, 4, 7, 4, 8, 3, 6, 4, 8};
-            }
+            vector<short> max = {2, 1, 4, 7, 4, 8, 3, 6, 4, 7};
             
             for(int i = 0; i < 10; i++)
             {
@@ -48,6 +41,6 @@ public:
             rev_x = rev_x * 10 + digits_vec[i];
         }
 
-        return rev_x * ( x / abs(x) );
+        return rev_x * sign ;
     }
 };
